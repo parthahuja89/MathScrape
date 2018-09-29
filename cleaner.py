86a5f2dd87b647f4677ce58f89caf69dc7b5922d
parthahuj@gmail.com <parthahuj@gmail.com>
2018-09-29 17:55:57 -0400

    1) Added front end files -Vue 2) Added restful flask server

--- cleaner.py
+++ cleaner.py
@@ -26,15 +26,17 @@ dictionary.append('sqrt')
 def cleaner(expression):
 
     ex = list(expression)
-    cleaned = []
+    cleaned = list()
 
     for e in ex:
+
         if(isSqrt(e,ex)):
             cleaned.append('sqrt')
         if(isOver(e,ex)):
-           currentidx = ex.index(e)
-           cleaned.__add__(')', currentidx-1)
-           cleaned.__add__("(", 0)
+          #replace at first and move further
+         cleaned.insert(0, '(')
+         cleaned.append(')/')
+
         if (e in dictionary or e.isdigit()):
             cleaned.append(e)
 
@@ -56,8 +58,9 @@ def isOver(current, arr):
             return True
 
 if __name__ == "__main__":
-    e = '{-b\pm\sqrt{b^2-4ac} \over 2a}. '
-    print('original string:' + e)
-    cleaner(e)
-    cleaned_string = ''.join(map(str, cleaner(e)))
+    e = '{-b\pm\sqrt{b^2-4ac}\over2a}. '
+    e2 = 'xhelloooooo^43+yhfe'
+    print('original string:' + e2)
+    cleaner(e2)
+    cleaned_string = ''.join(map(str, cleaner(e2)))
     print(cleaned_string)
