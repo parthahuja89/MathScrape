aea70a9b2e00fc9071df73914734b456c350229d
parthahuj@gmail.com <parthahuj@gmail.com>
2018-10-20 14:38:20 -0400

    changing CORS

--- back_end/server.py
+++ back_end/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 import solo
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
@@ -8,14 +8,17 @@ app.config.from_object(__name__)
 
            
 CORS(app) #different origins 
-
+app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/', methods = ['GET'])
 def front():
     print('Hello Wold!')
+    return 'yaya'
+
 #reqquests 
-@app.route('/send_url', methods=['POST', 'GET', 'OPTIONS'])
+@app.route('/send_url', methods=['POST', 'GET'])
+@cross_origin()
 def ping_pong():
-     if request.method == 'POST, OPTIONS':
+     if request.method == 'POST':
          response = request.get_json()
          url = response.get('url')
          soup =solo.setup(url)
